f6c1c56172fc14f9b394f614876dd2275e71621c
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-02-13 13:12:19 -0600

    Added the new method to clean the text field

--- app/src/main/java/com/ualr/firstapp/MainActivity.java
+++ app/src/main/java/com/ualr/firstapp/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.ualr.firstapp.databinding.ActivityMainBinding;
 import android.text.TextUtils;
 import android.widget.TextView;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,14 +28,39 @@ public class MainActivity extends AppCompatActivity {
             // Change hint if user has not entered a message
             TextView textView = findViewById(R.id.userInputET);
             textView.setHint("You must type something first.");
-
         }
         else{
             // Only run this line to swap the text if the user has entered a message
             mBinding.userMsgTV.setText(mBinding.userInputET.getText().toString());
         }
-
     }
 
+
     // TODO 07. Create a new method called cleanTextField to delete the text inside the text field
+    public void cleanTextField(View view){
+        //clean text inside the text field
+
+        // If text field is empty, there is nothing to clean
+        if(TextUtils.isEmpty(mBinding.userInputET.getText().toString())){
+            TextView textView = findViewById(R.id.userInputET);
+            textView.setHint("Text field already clean.");
+
+            // Handler - adds a delay before setting the hint back
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // code to be executed after a specified time interval
+                    textView.setHint("You must type something first.");
+                }
+            }, 5000); // 5000 milliseconds = 5 seconds
+
+        }
+        // If text field is not empty, clean it out
+        else{
+            // Remember to change userInputET to the new ID
+            mBinding.userMsgTV.setText(mBinding.userInputET.getText().toString());
+        }
+
+    }
 }
